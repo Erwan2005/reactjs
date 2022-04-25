b92faff606416b5b6886366db7f6899d9a3c1d21
erwan <manohisoaguyaimar@gmail.com>
2022-04-25 06:41:46 +0300

    latest at time

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const BASE_URL = "http://127.0.0.1:8000/";
+export const BASE_URL = "https://guy221293.pythonanywhere.com/";
 // const TOKEN =
 //   JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser
 //     .accessToken || "";

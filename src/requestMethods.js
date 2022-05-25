80f3693b9fdc2890593e5cbff88864289379e30c
erwan <manohisoaguyaimar@gmail.com>
2022-05-25 22:18:23 +0300

    mai 26 2022

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const BASE_URL = "http://localhost:8000/";
+export const BASE_URL = "https://guy221293.pythonanywhere.com/";
 // const TOKEN =
 //   JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser
 //     .accessToken || "";

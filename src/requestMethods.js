04e221578fddd0be40193a8f596025425511b07d
erwan <manohisoaguyaimar@gmail.com>
2022-07-21 22:16:48 +0300

    advanced

---
+++ src/requestMethods.js
@@ -0,0 +1,30 @@
+import axios from "axios";
+const url = "https://guy221293.pythonanywhere.com/";
+export const BASE_URL = url;
+// const TOKEN =
+//   JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser
+//     .accessToken || "";
+
+const user = JSON.parse(localStorage.getItem("persist:root"))?.user;
+const currentUser = user && JSON.parse(user).currentUser;
+const TOKEN = currentUser?.token;
+console.log(user)
+export const publicRequest = axios.create({
+  baseURL: BASE_URL,
+});
+
+export const parseRequest = axios.create({
+  baseURL: BASE_URL,
+  headers: {
+    "content-type": "multipart/form-data",
+    "Authorization": `Token ${TOKEN}`, 
+  }
+});
+
+export const userRequest = axios.create({
+  baseURL: BASE_URL,
+  headers: { 
+    "Authorization": `Token ${TOKEN}`, 
+  },
+});
+
